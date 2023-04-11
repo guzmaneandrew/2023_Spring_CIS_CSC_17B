491a1002b2cbb4c851c56e34b92e52abe0df2ef3
Andrew G <guzmanandrewe@gmail.com>
2023-04-11 14:23:41 -0700

    Create User class: Start creating functions for User class - Copied functions from Admin class and modifying for User

---
+++ Project/Survey2/S9User/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=s9user
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/s9user
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=s9user.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/s9user.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=s9user
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/s9user
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=s9user.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/s9user.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
