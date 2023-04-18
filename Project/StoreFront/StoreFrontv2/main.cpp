8c1c5e99106f486c3c88bbabae53b537184aeb86
Andrew G <guzmanandrewe@gmail.com>
2023-04-17 20:19:09 -0700

    Add files for 4/03 lecture code (Compare Objects,Arrays,Functions)

---
+++ Project/StoreFront/StoreFrontv2/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Guzman
+ * Created: April 17, 2023 @ 1:36 AM
+ * Purpose: v1: Item class
+ */
+
+//System and User Libraries
+#include "Headers.h"
+
+//Global Constants
+//Physics/Chemistry/Math/Conversion Higher Dimension Only
+
+//Structure Definitions
+
+//Function Prototypes
+
+//Program Execution Begins Here!!!
+
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+    Item item("Dragonfruit",3.69,50);
+    item.display();
+    
+    //Initial Variables
+
+    //Map the Inputs to the Outputs
+
+    //Display the Inputs and Outputs
+
+    //Clean Up the Dynamic Stuff
+
+    //Exit 
+    return 0;
+}
+
