9a84c612c4e6ad4b02a9c516566d58491d474573
Andrew G <guzmanandrewe@gmail.com>
2023-04-19 01:49:35 -0700

    Admin class: - added functionality to add and view items in the store - added binary file operations to store items

---
+++ Project/StoreFront/StoreFront2Store/Store.h
@@ -0,0 +1,178 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   Store.h
+ * Author: andrewguzman
+ *
+ * Created on April 17, 2023, 2:04 AM
+ */
+
+#ifndef STORE_H
+#define STORE_H
+#include "Headers.h"
+
+class Store {
+private:
+    vector<Item *> items;
+    int numItems;
+public:
+
+    Store() {
+    }
+
+    Store(int num) {
+        numItems = num;
+        for (int i = 0; i < num; i++) {
+            newItem();
+        }
+    }
+
+    ~Store() {
+        for (int i = 0; i < items.size(); i++) {
+            delete items[i];
+        }
+    }
+
+    int getNumItms() const {
+        return numItems;
+    }
+
+    vector<Item *> getItems() const {
+        return items;
+    }
+
+    Item *getItem(int i) const {
+        return items[--i];
+    }
+
+    void setItems(vector<Item *> items) {
+        this->items = items;
+    }
+
+    void setNumItems(int n) {
+        numItems = n;
+    }
+
+    void setNumItems() {
+        numItems = items.size();
+    }
+
+    void display() const {
+        cout << endl << "Items in Store" << endl;
+        for (int i = 0; i < items.size(); i++) {
+            items[i]->display();
+            cout << endl;
+        }
+    }
+
+    void newItem() {
+        //Receive input for new item
+        string name;
+        float price;
+        int stock;
+
+        cout << "Item Name: ";
+        getline(cin, name);
+
+        cout << "Item Price: $";
+        cin>>price;
+        cin.ignore();
+
+        cout << "Number in Stock: ";
+        cin>>stock;
+        cin.ignore();
+
+        //Create new Item object
+        Item *item = new Item(name, price, stock);
+        items.push_back(item);
+    }
+
+    void deleteItem(int i) {
+        int index=--i;
+        if (index >= 0 && index < items.size()) {
+            numItems--;
+            delete items[index];
+            items.erase(items.begin() + index);
+        } else {
+            cout << "ERROR: Invalid Item Number." << endl;
+        }
+    }
+
+    void saveBin(string file) {
+        fstream storeDB(file, ios::out | ios::binary | ios::app);
+
+        if (!storeDB) {
+            cerr << "Error: Unable to Open File for Writing." << endl;
+            return;
+        }
+
+        //Write number of items to binary file
+        storeDB.write(reinterpret_cast<char *> (&numItems), sizeof (int));
+
+        //Write each item info to binary file
+        for (int i = 0; i < numItems; i++) {
+            string name;
+            float price;
+            int numStck;
+
+            name = items[i]->getName();
+            price = items[i]->getPrice();
+            numStck = items[i]->getStock();
+
+            int len = name.length();
+            storeDB.write(reinterpret_cast<char *> (&len), sizeof (int));
+            storeDB.write(name.c_str(), len);
+            storeDB.write(reinterpret_cast<char *> (&price), sizeof (float));
+            storeDB.write(reinterpret_cast<char *> (&numStck), sizeof (int));
+        }
+        storeDB.close();
+    }
+
+    void readBin(string file) {
+        //Clear items from vector
+        items.clear();
+        
+        fstream storeDB(file, ios::in | ios::binary);
+
+        if (!storeDB) {
+            cerr << "Error: Unable to Open File for Writing." << endl;
+            return;
+        }
+
+        //Read number of items from binary file
+        int num;
+        storeDB.read(reinterpret_cast<char *> (&num), sizeof (int));
+
+        //Read each of the item's properties in the file
+        for (int i = 0; i < num; i++) {
+            int len;
+            string name;
+            float price;
+            int numStck;
+
+            storeDB.read(reinterpret_cast<char *> (&len), sizeof (int));
+            name.resize(len);
+            storeDB.read(&name[0], len);
+            storeDB.read(reinterpret_cast<char *> (&price), sizeof (float));
+            storeDB.read(reinterpret_cast<char *> (&numStck), sizeof (int));
+
+            //Create new Item object and set it's properties
+            Item *newItem = new Item(name, price, numStck);
+            items.push_back(newItem);
+        }
+
+        //Create new Store object, set the items, and display
+        Store *store = new Store();
+        store->setItems(items);
+        store->display();
+
+        storeDB.close();
+    }
+};
+
+
+#endif /* STORE_H */
+
---
+++ Project/StoreFront/StoreFront3Admin/Admin.h
@@ -0,0 +1,237 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   Admin.h
+ * Author: andrewguzman
+ *
+ * Created on April 18, 2023, 2:08 PM
+ */
+
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "Store.h"
+
+struct AdminInfo {
+    char username[10];
+    char password[12];
+};
+
+class Admin {
+private:
+    AdminInfo info;
+    Store *store;
+public:
+
+    Admin(string name, string pass) {
+        strcpy(info.username, name.c_str());
+        strcpy(info.password, pass.c_str());
+        store = new Store();
+
+        int choice;
+        do {
+            cout << endl << "Hello Admin, " << info.username << endl;
+            cout << " 1) Add New Items" << endl;
+            cout << " 2) View Items" << endl;
+            cout << " 3) View Items Currently In Stock" << endl;
+            cout << " 4) View Items No Longer In Stock" << endl;
+            cout << " 5) Update an Item" << endl;
+            cout << " 6) List Users" << endl;
+            cout << " 7) List Admins" << endl;
+            cout << " 8) Add Admins" << endl;
+            cout << "-99) Delete All Items" << endl;
+            cout << "-1) Logout" << endl;
+            cin>>choice;
+            cin.ignore();
+            if (choice == 1) {
+                addItems();
+            } else if (choice == 2) {
+                viewItms();
+            } else if (choice == 3) {
+                viewItmsInStck();
+            } else if (choice == 4) {
+                viewItmsNotInStck();
+            } else if (choice == 5) {
+                updtItem();
+            } else if (choice == 6) {
+                listUsrs();
+            } else if (choice == 7) {
+                listAdmins();
+            } else if (choice == 8) {
+                addAdmin();
+            } else if (choice == -99) {
+                deleteAll();
+            }
+
+        } while (choice != -1);
+    }
+
+    ~Admin() {
+        delete store;
+    }
+
+    void addItems() {
+        //Create items and add to item DB
+        int num;
+
+        cout << "ADD ITEMS" << endl;
+        cout << "Enter Number of Items: ";
+        cin>>num;
+        cin.ignore();
+
+        store->setItems(num);
+        store->saveBin("Store.dat");
+        store->display();
+    }
+
+    void viewItms() {
+        readBin("Store.dat");
+
+        cout << "ALL STORE ITEMS" << endl;
+        if (store->getNumItms() == 0) {
+            cout << "No Items to Retrieve from the Database." << endl;
+        } else {
+            for (int i = 0; i < store->getNumItms(); i++) {
+                store->getItem(i)->display();
+            }
+        }
+    }
+
+    void viewItmsInStck() {
+        bool none = true;
+        int num = 1; //number in the list and number of items in stock
+
+        readBin("Store.dat");
+
+        cout << endl << "STORE ITEMS CURRENTLY IN STOCK" << endl;
+        if (store->getNumItms() == 0) {
+            cout << "No Items to Retrieve from the Database." << endl;
+        } else {
+            for (int i = 0; i < store->getNumItms(); i++) {
+                if (store->getItem(i)->getStock() != 0) {
+                    cout << num++ << ") " << endl;
+                    store->getItem(i)->display();
+                    none = false;
+                }
+            }
+            if (none == true) {
+                cout << "- - - - - - None - - - - - -" << endl;
+            } else {
+                cout << "Total Items in Stock: " << --num << "/" << store->getNumItms() << endl;
+            }
+        }
+    }
+
+    void viewItmsNotInStck() {
+        bool none = true;
+        int num = 1; //number in the list and number of items in stock
+
+        readBin("Store.dat");
+
+        cout << endl << "STORE ITEMS CURRENTLY NOT IN STOCK" << endl;
+        if (store->getNumItms() == 0) {
+            cout << "No Items to Retrieve from the Database." << endl;
+        } else {
+            for (int i = 0; i < store->getNumItms(); i++) {
+                if (store->getItem(i)->getStock() == 0) {
+                    cout << num++ << ") " << endl;
+                    store->getItem(i)->display();
+                    none = false;
+                }
+            }
+            if (none == true) {
+                cout << "- - - - - - None - - - - - -" << endl;
+            } else {
+                cout << "Total Items Not in Stock: " << --num << "/" << store->getNumItms() << endl;
+            }
+        }
+    }
+
+    void updtItem() {
+
+    }
+
+    void listUsrs() {
+
+    }
+
+    void listAdmins() {
+
+    }
+
+    void addAdmin() {
+
+    }
+
+    void deleteAll() {
+        clearBin("Store.dat");
+        store->clearItems();
+        cout << "All Items Have Been Deleted from Store" << endl;
+    }
+
+    void readBin(string file) {
+        store->clearItems();
+
+        fstream db(file, ios::in | ios::binary);
+        if (!db) {
+            cerr << "ERROR: Unable to Open File for Reading." << endl;
+            return;
+        }
+
+        //Use cursor to get size of file to read entire file
+        db.seekg(0, ios::end);
+        int size = db.tellg();
+        db.seekg(0, ios::beg);
+
+        vector<Item *> items;
+        //Read in item information until size of file reached
+        while (db.tellg() < size) {
+            //Read number of items
+            int numItms;
+            db.read(reinterpret_cast<char *> (&numItms), sizeof (int));
+            for (int i = 0; i < numItms; i++) {
+                int len;
+                string name;
+                float price;
+                int numStck;
+
+                db.read(reinterpret_cast<char *> (&len), sizeof (int));
+                name.resize(len);
+                db.read(&name[0], len);
+                db.read(reinterpret_cast<char *> (&price), sizeof (float));
+                db.read(reinterpret_cast<char *> (&numStck), sizeof (int));
+
+                //Create new Item object and set it's properties
+                Item *newItem = new Item(name, price, numStck);
+                items.push_back(newItem);
+            }
+
+        }
+        //Set store items vector
+        store->setItems(items);
+        store->setNumItems(items.size());
+
+        db.close();
+    }
+
+    void clearBin(string file) {
+        //Deletes all contents of the binary file
+        ofstream DB(file, ios::out | ios::trunc);
+        DB.close();
+    }
+
+    void reloadItms(string file) {
+        //Clear binary file, output vector to binary, then read from binary
+        clearBin(file);
+
+        store->saveBin(file);
+
+        readBin(file);
+    }
+};
+
+#endif /* ADMIN_H */
+
