05ac1d2284fe01389455b22de70e07b4dfef1863
Andrew G <guzmanandrewe@gmail.com>
2023-05-05 00:50:58 -0700

    Minor update to Survey Engine project

---
+++ Project/StoreFront/StoreFront5Cart_1/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Guzman
+ * Created: April 17, 2023 @ 1:36 AM
+ * Purpose: v2: Store class
+ */
+
+//System and User Libraries
+#include "Headers.h"
+
+//Global Constants
+//Physics/Chemistry/Math/Conversion Higher Dimension Only
+
+//Structure Definitions
+
+//Function Prototypes
+
+//Program Execution Begins Here!!!
+
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+//    Admin admin("Andrew","1234");
+    User user;
+    user.setUsrInfo("Andrew","1234");
+    user.prompt();
+    //Initial Variables
+
+    //Map the Inputs to the Outputs
+
+    //Display the Inputs and Outputs
+
+    //Clean Up the Dynamic Stuff
+
+    //Exit 
+    return 0;
+}
+
